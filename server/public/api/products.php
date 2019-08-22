bf285812ef904b9eb460db3f4985d7323d03e7a2
Tesia Tran <tesiatran88@gmail.com>
2019-08-22 15:48:50 -0700

    Completed featureset 1: Make initial products endpoint and deliver dummy data. (Back-end)

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,11 +1,12 @@
 <?php
+  require_once "functions.php";
 
-header('Content-Type: application/json');
+  set_exception_handler("error_handler");
 
-if (empty($_GET['id'])) {
-  readfile('dummy-products-list.json');
-} else {
-  readfile('dummy-product-details.json');
-}
+  $output = file_get_contents("dummy-products-list.json");
+
+  print($output);
+
+  doStuff();
 
 ?>
