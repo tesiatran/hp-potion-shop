9737e871d6b58e54b6e885f6e3ede07ddfde0078
Tesia Tran <tesiatran88@gmail.com>
2019-08-22 11:11:40 -0700

    Defined a stateless ProductListItem component in client/components/product-list-item.jsx that renders a single product card.

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './header';
+import ProductListItem from './product-list-item';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <Header text="Wicked Sales"/>
+        <ProductListItem/>
       </div>
     );
   }
---
+++ client/components/product-list-item.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ProductListItem(props) {
+  return (
+    <div>{props.card}</div>
+  );
+}
+
+export default ProductListItem;
