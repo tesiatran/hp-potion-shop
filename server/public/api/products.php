8e09dd137e117e8553f21369ae10dd28d1794b9a
Tesia Tran <tesiatran88@gmail.com>
2019-08-22 17:41:30 -0700

    Completed featureset 2: Create connection file for MySQL. (Back-end)

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -3,6 +3,12 @@
 
   set_exception_handler("error_handler");
 
+  require_once "db_connection.php";
+
+  if($conn) {
+    throw new Exception("ERROR: ", mysqli_connect_error());
+  };
+
   $output = file_get_contents("dummy-products-list.json");
 
   print($output);
