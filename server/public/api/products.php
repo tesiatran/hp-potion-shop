d8d7bb5b8103c5feecc25875d68cd213fe99ca69
Tesia Tran <tesiatran88@gmail.com>
2019-08-22 20:27:28 -0700

    Completed featureset 4: Connect to products table and fetch data - access products data. (Back-end)

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,17 +1,32 @@
 <?php
 
   require_once "functions.php";
-
   set_exception_handler("error_handler");
+  startup();
 
   require_once "db_connection.php";
-
-  if($conn) {
-    throw new Exception("ERROR: ", mysqli_connect_error());
+  if(!$conn) {
+    throw new Exception("ERROR: " . mysqli_connect_error());
   };
 
-  $output = file_get_contents("dummy-products-list.json");
+  $query = "SELECT * FROM `products`";
 
-  print($output);
+  $result = mysqli_query($conn, $query);
 
+  if(!$result) {
+    throw new Exception("ERROR: " . mysqli_error($conn));
+    exit();
+  }
+
+  $output = [
+    "success" => "true",
+    "data" => []
+  ];
+
+  while($product = mysqli_fetch_assoc($result)) {
+    $output["data"][] = $product;
+  }
+
+  $json_output = json_encode($output["data"]);
+  print($json_output);
 ?>
