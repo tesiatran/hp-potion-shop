b8a5b0a190cf198535ac4cbe7794e82be11554d4
Tesia Tran <tesiatran88@gmail.com>
2019-08-28 16:57:37 -0700

    Defined stateless CartSummaryItem component that displays item image, name, price, short description. Defined stateless CartSummary component that displays a CartSummaryItem for now.

---
+++ client/components/cart-summary-item.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function CartSummaryItem(props) {
+  return (
+    <div className="card col-3 mx-2 my-2">
+      <img src={props.productData.image} className="img-fluid my-2"></img>
+      <div className="font-weight-bold my-1">{props.productData.name}</div>
+      <div className="my-1">${(props.productData.price * 0.01).toFixed(2)}</div>
+      <div className="my-1">{props.productData.shortDescription}</div>
+    </div>
+  );
+}
+
+export default CartSummaryItem;
---
+++ client/components/cart-summary.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CartSummaryItem from './cart-summary-item';
+
+function CartSummary() {
+  return (
+    <CartSummaryItem/>
+  );
+}
+
+export default CartSummary;
