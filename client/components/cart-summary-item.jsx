77a24e812efa2a605033227227016cb77239ba6e
Tesia Tran <tesiatran88@gmail.com>
2019-08-28 23:11:58 -0700

    Total price of all items in cart displays correctly. Added styling to CartSummary page so things are aligned and centered.

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 
 function CartSummaryItem(props) {
   return (
-    <div className="container">
-      <div className="row">
-        <img src={props.cartItemData.image} className="col my-3"></img>
-        <div className="col">
-          <div className="row">
-            <div className="col justify-content-start productName my-3">{props.cartItemData.name}</div>
+    <div className="container col-12">
+      <div className="card col-6 d-block mx-auto">
+        <div className="row">
+
+          <div className="col">
+            <img src={props.cartItemData.image} className="card-img"></img>
           </div>
-          <div className="row">
-            <div className="col justify-content-start my-3">${(props.cartItemData.price * 0.01).toFixed(2)}</div>
+
+          <div className="col">
+            <div className="card-body">
+              <div className="card-title justify-content-start productName my-3">{props.cartItemData.name}</div>
+              <div className="card-text justify-content-start my-3">${(props.cartItemData.price * 0.01).toFixed(2)}</div>
+              <div className="card-text justify-content-start my-3">{props.cartItemData.shortDescription}</div>
+            </div>
           </div>
-          <div className="row">
-            <div className="col justify-content-start my-3">{props.cartItemData.shortDescription}</div>
-          </div>
+
         </div>
       </div>
     </div>
--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import CartSummaryItem from './cart-summary-item';
 
 function CartSummary(props) {
+  var totalPrice = props.cartTotalData
+    .map(item => item.price)
+    .reduce((a, b) => a + b, 0);
+
   return (
     <div className="container">
       <div onClick={() => {
         props.setView('catalog', {});
       }}>{props.backText}</div>
-      <div>My Cart</div>
-      <CartSummaryItem cartItemData={props.cartTotalData}/>
-      <div>Total</div>
+      <h1 className="my-3 col-6 mx-auto text-left">My Cart</h1>
+      <div className="row my-3">
+        {props.cartTotalData.map(item => {
+          return (
+            <CartSummaryItem key={item.id} cartItemData={item}/>
+          );
+        })}
+      </div>
+      <h1 className="my-5 col-6 mx-auto text-left">Total: ${(totalPrice * 0.01).toFixed(2)}</h1>
     </div>
   );
 }
