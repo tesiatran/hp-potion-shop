31277da04d3d981d4ca52ed9910478e81a4740f6
Tesia Tran <tesiatran88@gmail.com>
2020-02-10 13:20:27 -0800

    Changed if statement to switch to account for other request methods.

--- server/public/api/cart.php
+++ server/public/api/cart.php
@@ -7,17 +7,27 @@ require_once('db_connection.php');
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-if ($method == 'GET') {
-  readfile('dummy-cart-items.json');
-} else if ($method == 'POST') {
-  http_response_code(201);
-  print($item);
-} else {
-  http_response_code(404);
-  print(json_encode([
-    'error' => 'Not Found',
-    'message' => "Cannot $method /api/cart.php"
-  ]));
+switch($method) {
+  case 'GET':
+    http_response_code(200);
+    require_once('cart-get.php');
+    break;
+  case 'POST':
+    http_response_code(201);
+    require_once('cart-add.php');
+    break;
+  case 'DELETE':
+    require_once('cart-delete.php');
+    break;
+  case 'PUT':
+    require_once('cart-update.php');
+    break;
+  default:
+    http_response_code(404);
+    print(json_encode([
+      'error' => 'Not Found',
+      'message' => "Cannot $method /api/cart.php"
+    ]));
 }
 
 ?>
