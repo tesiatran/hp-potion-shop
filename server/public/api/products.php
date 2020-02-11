6dc7920e8fb0a494cbf296e5775e2364722ca385
Tesia Tran <tesiatran88@gmail.com>
2020-02-11 02:20:19 -0800

    Updated syntax to be consistent.

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,9 +1,10 @@
 <?php
-  require_once "functions.php";
-  set_exception_handler("error_handler");
+
+  require_once('functions.php');
+  require_once('db_connection.php');
+  set_exception_handler('error_handler');
   startup();
 
-  require_once "db_connection.php";
   if(!$conn) {
     throw new Exception("ERROR: " . mysqli_connect_error());
   };
