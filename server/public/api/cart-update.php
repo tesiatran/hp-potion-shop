d8fea9e8d59edb275463820daf52507077bc9149
Tesia Tran <tesiatran88@gmail.com>
2020-02-11 01:50:57 -0800

    Check to see if ['id'] exists. Then error check to see if it's in the right format. Otherwise throw exception.

--- server/public/api/cart-update.php
+++ server/public/api/cart-update.php
@@ -10,4 +10,16 @@ if (!INTERNAL) {
 $item = file_get_contents('php://input');
 $jsonData = getBodyData($item);
 
+if ($jsonData['id']) {
+  $id = $jsonData['id'];
+  if (gettype($id) !== 'integer') {
+    throw new Exception('ID must be a number');
+  }
+  if (intval($id) < 1) {
+    throw new Exception('ID must be a number greater than 0');
+  }
+} else {
+  throw new Exception('ID is required to update cart');
+}
+
 ?>
