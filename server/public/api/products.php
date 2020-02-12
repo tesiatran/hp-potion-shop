31c1fb731dbbb07905e3d96a5535d89e3a573909
Tesia Tran <tesiatran88@gmail.com>
2020-02-12 12:59:57 -0800

    Called startUp function in products.php file.

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -5,37 +5,40 @@
   set_exception_handler('error_handler');
   startUp();
 
-  if (empty($_GET['id'])) {
-    $whereClause = "";
-  } else {
-    $id = $_GET['id'];
-    $whereClause = "WHERE products.id = {$id}";
-    if(!is_numeric($id)) {
-      throw new Exception('ID must be a number');
-    }
-  };
+  // if (empty($_GET['id'])) {
+  //   $whereClause = "";
+  // } else {
+  //   $id = $_GET['id'];
+  //   $whereClause = "WHERE products.id = {$id}";
+  //   if(!is_numeric($id)) {
+  //     throw new Exception('ID must be a number');
+  //   }
+  // };
 
-  $query = "SELECT * FROM `products`" . $whereClause;
-  $result = mysqli_query($conn, $query);
+  // $query = "SELECT * FROM `products`" . $whereClause;
+  // $result = mysqli_query($conn, $query);
 
-  if (!$result) {
-    throw new Exception("ERROR: " . mysqli_error($conn));
-    exit();
-  }
+  // if (!$result) {
+  //   throw new Exception("ERROR: " . mysqli_error($conn));
+  //   exit();
+  // }
 
-  $rows = mysqli_num_rows($result);
+  // $rows = mysqli_num_rows($result);
 
-  if (!$rows) {
-    throw new Exception("Invalid ID: {$id}");
-  }
+  // if (!$rows) {
+  //   throw new Exception("Invalid ID: {$id}");
+  // }
 
-  $output = [];
+  // $output = [];
 
-  while ($product = mysqli_fetch_assoc($result)) {
-    $output[] = $product;
-  }
+  // while ($product = mysqli_fetch_assoc($result)) {
+  //   $output[] = $product;
+  // }
 
-  $json_output = json_encode($output);
-  print($json_output);
+  // $json_output = json_encode($output);
+  // print($json_output);
+
+  $output = file_get_contents('dummy-products-list.json');
+  print($output);
 
 ?>
