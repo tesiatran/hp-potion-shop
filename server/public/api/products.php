d4f77294e1c92895570669e8da41bd2306380d97
Tesia Tran <tesiatran88@gmail.com>
2020-02-12 19:57:11 -0800

    Endpoint is working, but Postman is still not showing JSON body. Possibly a problem with the encoding?

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,53 +1,41 @@
 <?php
 
-  require_once('functions.php');
-  require_once('db_connection.php');
-  set_exception_handler('error_handler');
+  require_once("functions.php");
+  require_once("db_connection.php");
+  set_exception_handler("error_handler");
   startUp();
 
-  $query = "SELECT * FROM `products`";
-  $result = mysqli_query($conn, $query);
+  if (empty($_GET["id"])) {
+    $whereClause = "";
+  } else if (is_numeric($_GET["id"])) {
+    $whereClause = "WHERE products.id = " . $_GET["id"];
+  } else {
+    throw new Exception("ID must be a number");
+  }
 
-  // var_dump($result);
+  $query = "SELECT * FROM `products`" . $whereClause;
+  $result = mysqli_query($conn, $query);
+  $rows = mysqli_num_rows($result);
 
   if (!$result) {
     throw new Exception("ERROR: " . mysqli_error($conn));
-    exit();
   }
 
-  // if (empty($_GET['id'])) {
-  //   $whereClause = "";
-  // } else {
-  //   $id = $_GET['id'];
-  //   $whereClause = "WHERE products.id = {$id}";
-  //   if(!is_numeric($id)) {
-  //     throw new Exception('ID must be a number');
-  //   }
-  // };
-
-  // $query = "SELECT * FROM `products`" . $whereClause;
-  // $result = mysqli_query($conn, $query);
-
-  // $rows = mysqli_num_rows($result);
-
-  // if (!$rows) {
-  //   throw new Exception("Invalid ID: {$id}");
-  // }
-
-  $output = [
-    'success' => 'true',
-    'data' => []
-  ];
-
-  // var_dump($output);
-
-  while ($product = mysqli_fetch_assoc($result)) {
-    $output['data'][] = $product;
+  if ($rows === 1) {
+    $output = mysqli_fetch_assoc($result);
+    $product = $output;
+  } else {
+    $output = [];
+    while ($product = mysqli_fetch_assoc($result)) {
+      $output[] = $product;
+    }
   }
 
-  $json_output = json_encode($output);
-  print($json_output);
+  // print_r($output);
 
+  print(json_encode($output));
+
+  // print_r($output);
 
   // $output = file_get_contents('dummy-products-list.json');
   // print($output);
