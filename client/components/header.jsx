ab469fdca67e4c6522c87d10ae93b73a6bb73cec
Tesia Tran <tesiatran88@gmail.com>
2020-02-13 20:42:20 -0800

    Updated store logo. Renamed image.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(props) {
   return (
     <header className="row align-items-center justify-content-center">
-      <img className="logo rounded-circle" src="images\ws.jpg"/>
+      <img className="logo" src="images\general\snow-logo.png"/>
       <h1 className="storeName">{props.nameText}</h1>
       <div className="text cart mx-5" onClick={() => { props.setView('cart', {}); }}>{props.cartItemCount} Items <i className="fas fa-shopping-cart" onClick={() => { props.setView('cart', {}); }}></i></div>
     </header>
